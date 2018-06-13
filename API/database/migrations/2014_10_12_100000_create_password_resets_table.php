0f8768dc5186418ad5e6100c07b20153b67c58e0
stephan_mai <stephan@coderebels.de>
2018-06-13 12:07:02 +0200

    migrations sql_5.6

--- API/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ API/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,8 +14,8 @@ class CreatePasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
+            $table->string('email')->collation('utf8_unicode_ci')->index();
+            $table->string('token')->collation('utf8_unicode_ci');
             $table->timestamp('created_at')->nullable();
         });
     }
